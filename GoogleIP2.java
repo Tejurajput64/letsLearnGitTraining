681dd994121b37cccac6857d590506e2d2f4afec
Tejaswini <Tejaswini@gmail.com>
2022-08-25 21:29:16 -0700

    This file adding from eclipse.

---
+++ GoogleIP2.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.io.*;
+class GoogleIP2
+{
+	public static void main(String[]args)
+	{
+		try
+		{
+		InetAddress ip= InetAddress.getByName("www.Google.com");
+		System.out.println("modify here=" +ip.getHostName());
+		System.out.println("ip=" +ip.getHostAddress());
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
