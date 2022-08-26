b3366769cd42cf3b65395e2ec7dfd86d5ec24602
Tejaswini <Tejaswini@gmail.com>
2022-08-25 20:35:04 -0700

    I am adding new file to qa1

---
+++ Actevent2.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class Actevent2 extends JFrame implements ActionListener
+{
+	TextField tf;
+	
+	Actevent2()
+	{
+		//JLabel l1=new JLabel("www.google.com");
+		//l1.setBounds(100,50,100,30);
+
+		JTextField t1=new JTextField("www.google.com");
+		t1.setBounds(100,85,100,30);
+	
+		Button b=new Button("Display IP");
+		b.setBounds(100,120,100,30);
+		b.addActionListener(this);
+		
+		add(t1);
+		//add(l1);
+		add(b);
+		
+		setSize(400,400);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		t1.setText("IP address is");	
+	}
+	public static void main(String[]args)
+	{
+		new Actevent2();
+	}
+}
